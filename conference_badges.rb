492e75714a3469e106c688f9a6d7714d70e7da7e
Javier Aguilar <coyote176@msn.com>
2020-08-03 22:23:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,6 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 def batch_badge_creator["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  batch_badge_creator.each do |name|
+    return ""
   
